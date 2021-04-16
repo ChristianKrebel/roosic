79e20a73f698e897f8c9df8c0ab26f32ee69e348
Lukas <lukas.weidich@gmx.de>
2021-04-16 12:54:22 +0200

    ⚗ extend pwa config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withImages = require("next-images");
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA(
 	withImages({
 		pwa: {
 			dest: "public",
+			runtimeCaching,
 			disable: process.env.NODE_ENV === "development",
+			register: true,
 		},
 		future: { webpack5: true },
 	}),
